0d4d6062797bd9797dd872a2003769e0704741e2
monaSalih <monaalobisat@gmail.com>
2021-11-01 21:27:31 +0200

    up date

--- web_scraper2/scarper.py
+++ web_scraper2/scarper.py
@@ -22,14 +22,14 @@ def get_citations_needed_count(maxico_url):
     return (len(result),"result for length")
 
 def get_citations_needed_report(maxico_url):
-    text_arr=[]
+    clean_report=[]
     response = requests.get(maxico_url)
     html_text=response.text
     soup = BeautifulSoup(html_text, 'html.parser')
-    result = soup.find_all('a', title="Wikipedia:Citation needed")
-    # text_arr=text_arr.append(result)
-    # clean_report=[text_arr[i].parent.text for i in result]
-    
+    result = soup.find_all('sup', class_="noprint Inline-Template Template-Fact")
+    for i in result:
+        clean_report.append(i.parent.text.strip())
+
     return clean_report
 
 
