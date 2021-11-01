0464e5025e039b10556dc8f621ce3d8640190c8f
monaSalih <monaalobisat@gmail.com>
2021-11-01 16:03:13 +0200

    web-scraper

--- web_scraper2/scarper.py
+++ web_scraper2/scarper.py
@@ -4,5 +4,19 @@ from bs4 import BeautifulSoup
 domain ='https://en.wikipedia.org/'
 maxico_history = f'{domain}/wiki/History_of_Mexico'
 response = requests.get(maxico_history)
- 
+
+
 print(response.text)
+
+html_text=response.text
+
+with open("mexico_cite_quere.html",'w') as file:
+    file.write(html_text)
+print(html_text)
+
+soup = BeautifulSoup(html_text, 'html.parser')
+result = soup.find('div', id='results_inner_card')
+jobs = result.find_all('li', class_='has-pointer-d')
+
+def get_citations_needed_count(citations_contant):
+    title=citations_contant.find()
